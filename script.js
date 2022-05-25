21601641035ea5ebdf8a39497db50f6c65317065
Uria <dorman010@gmail.com>
2022-05-25 17:29:37 +0100

    cleaned up HTML, added JS query selectors

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// defines buttons, sets up initial display value of 0
+
+const displayValue = document.querySelector('#result');
+displayValue.innerHTML="<p>0</p>";
+const clearButton = document.querySelector('#clear')
+const deleteButton = document.querySelector('#delete');
+const numButtons = document.querySelectorAll('.num');
+const operatorButtons = document.querySelectorAll('.operator');
+const returnButton = document.querySelector('#finish');
+
+//capture keystrokes and presses, update display
+var number = number.addEventListener('keydown');
+//clear and delete buttons
+
+// Calculator functions, does not allow to divide by zero.
 function add(a,b){
     console.log(a+b);
     return a+b;
@@ -18,7 +33,7 @@ function multiply(a,b){
     console.log(a*b);
     return a*b;
 }
-
+//detects the operator and adds/subtracts
 function operate(a,b,operator){
     if (operator == "+") {
         add(a,b);
